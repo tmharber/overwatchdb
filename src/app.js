0b2cf6ce095e79f07bf88191ac18e55f92dbf83d
tmharber <tommharber@gmail.com>
2017-01-17 10:41:06 +0000

    initial hello world commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import AppRoutes from './components/appRoutes';
+
+render(
+  <AppRoutes/>, 
+  document.getElementById('main')
+);
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Route } from 'react-router'
+import IndexPage from './components/index';
+
+const routes = (
+ <Route path="/" component={IndexPage} />
+);
+
+export default routes;
